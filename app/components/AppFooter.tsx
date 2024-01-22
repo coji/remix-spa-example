4f8b1f92bda04d2db4d7c482782047d7ccc72bbd
coji <coji@techtalk.jp>
2024-01-22 11:30:43 +0900

    Update AppFooter component to include user avatar and improve UI

--- app/components/AppFooter.tsx
+++ app/components/AppFooter.tsx
@@ -3,7 +3,7 @@ import { UserIcon } from 'lucide-react'
 import { SignInModal } from '~/routes/auth+/sign_in'
 import { useSignOut } from '~/routes/auth+/sign_out'
 import { useAuthUser } from '~/services/auth'
-import { Button } from './ui'
+import { Avatar, AvatarFallback, AvatarImage, Button } from './ui'
 
 export const AppFooter = () => {
   const user = useAuthUser()
@@ -14,13 +14,26 @@ export const AppFooter = () => {
       <div className="mx-auto w-full px-6 xs:px-7 sm:px-10 max-w-screen-lg flex flex-col gap-11 md:flex-row">
         <div className="flex-1">
           {user ? (
-            <div>
-              <Link className="underline" to={`/${user.handle}`}>
-                <UserIcon className="inline mr-2" /> @{user.handle}
+            <div className="flex flex-col gap-4">
+              <Link
+                className="hover:underline flex gap-2 items-center"
+                to={`/${user.handle}`}
+              >
+                <Avatar>
+                  <AvatarImage src={user.photoURL ?? undefined} />
+                </Avatar>
+                @{user.handle}
               </Link>
-              <Button variant="link" onClick={() => signOut()}>
-                サインアウト
-              </Button>
+
+              <div>
+                <Button
+                  variant="link"
+                  className="p-0"
+                  onClick={() => signOut()}
+                >
+                  サインアウト
+                </Button>
+              </div>
             </div>
           ) : (
             <div className="gap-4 flex flex-col">
