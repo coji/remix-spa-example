85a6517ed80bb844a4862998f681d07d78b7f42c
coji <coji@techtalk.jp>
2024-06-05 21:38:06 +0900

    chore: Remove unused remix.config.ts file

--- remix.config.ts
+++
@@ -1,8 +0,0 @@
-import type { AppConfig } from '@remix-run/dev'
-import { flatRoutes } from 'remix-flat-routes'
-
-export default {
-  ignoredRouteFiles: ['**/*'],
-  routes: async (defineRoutes) => flatRoutes('routes', defineRoutes),
-  watchPaths: ['tailwind.config.js'],
-} satisfies AppConfig
